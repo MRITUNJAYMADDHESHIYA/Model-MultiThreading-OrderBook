9c6e400319f0a7e96078401273bc866d2225639c
Mritunjay Maddhesiya <mritunjay112002@gmail.com>
2025-10-15 20:35:49 +0530

    just do it

---
+++ Cpp_Fun/7_ZeroDivision.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(){
+    int x = 10;
+    int y = 0;
+    
+    std::cout<< x / y << std::endl;
+
+    double a = -10.0;
+    double b = 0.0;
+
+    std::cout<< a / b <<std::endl;
+    return 0;
+}
