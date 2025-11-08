9bd11ca6a95b0c2700db83c394203c5eb550823a
Mritunjay Maddhesiya <mritunjay112002@gmail.com>
2025-11-08 19:24:18 +0530

    sparse linear algebra method for a matrix

--- C++Lightning/1_SparseLinearAlgebra.cpp
+++ C++Lightning/1_SparseLinearAlgebra.cpp
@@ -4,3 +4,44 @@
 
 //3.save memory and speed up computations
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct CSRMatrix{
+    vector<double> values;
+    vector<int>    col_index;
+    vector<int>    row_ptr;
+    int rows, cols;
+
+    CSRMatrix(int r, int c): rows(r), cols(c){
+        row_ptr.push_back(0);
+    }
+
+    void addRow(const vector<pair<int, double>> &row){
+        for(auto [col, val]: row){
+            values.push_back(val);
+            col_index.push_back(col);
+        }
+        row_ptr.push_back(values.size());
+    }
+
+    void display(){
+        cout<<"values: ";
+        for(auto v: values) cout<< v <<" ";
+        cout<<"\ncol_index: ";
+        for(auto c: col_index) cout<< c << " ";
+        cout<<"\nrow_ptr: ";
+        for(auto r: row_ptr) cout<< r << " ";
+        cout<<endl;
+    }
+};
+
+int main(){
+    CSRMatrix m(3, 4);
+    m.addRow({{2,3}});
+    m.addRow({{0, 33}});
+    m.addRow({{0, 7}, {3, 5}});
+
+    m.display();
+}
