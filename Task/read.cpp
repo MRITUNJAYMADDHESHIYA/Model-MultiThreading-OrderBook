07b5b2c6fed697ed62898635cb98e7fa9bff31c9
Mritunjay Maddhesiya <mritunjay112002@gmail.com>
2025-12-03 09:30:25 +0530

    I try my best, let me know what next

--- Task/read.cpp
+++ Task/read.cpp
@@ -15,5 +15,21 @@
 ///2. Build a better model than the z-score trading system
 ///3. Compare and optimize the absolute P/L, sharpe ratio and Drawdown
 
-//I will write this python code in c++ after find good pair trading strategy in python using some different models
 
+/////////// Summary /////////////////
+//1. base.ipynb
+// ########### Pair Trading Strategy Class(some modification in zscore strategy then better perform) ##########
+// ########### 1.Modified exit logic to exit at mid-point of bands (midpoint should be dynamic, in this case (Upper+Lower)/2 But if I want for Better exit then I can do = spread mean, or Z=0. )
+// ########### 2.Added stop-loss based on implied volatility levels
+
+
+//2.myModel.ipynb
+//// I am using some model and try to optimize the above strategy based on regimes in the spread behavior
+// ## using RegimeDetectorGMM
+// # [1,2]---> 287.5, Sharpe Ratio: 6.04325228140793 (without model or)    -8025 trades
+// # 1    ---> 203,   Sharpe Ratio: 9.29770921355591 (high quality trades) -2056 trades-------->this is perfect for this
+// # 2    ---> 110,   Sharpe Ratio: 12.17756044604162                      -7141 trades
+
+// ## using RegimeDetectorARHMM
+// # [1] ---->193, Sharpe Ratio: 6.702584412675305                      -3656 trades
+// # [2] ---->99   Sharpe Ratio: 6.190348145217359                      -4439 trades
