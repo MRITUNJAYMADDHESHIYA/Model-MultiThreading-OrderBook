8f83e8bb1884b43658f41674230bc687ace5a3df
Mritunjay Maddhesiya <mritunjay112002@gmail.com>
2026-01-22 20:38:28 +0530

    23-01-2026 is new begining

--- test.cpp
+++ test.cpp
@@ -1,5 +1,9 @@
+///////This is my story till 22-01-2026, Now I am 24 years old. Now I am going to build quant strategy and trade in gold.
+
 //1.I am not working on my potential from last 3 months, I know I can do better, I am depressed, stress, overthinking all time thinking about my past and realizing my mistakes. But
 //i have to move on, I have to improve myself, I have to be better than yesterday. No matter how hard it is, I have to keep going.
+//I am scrolling social media too much, wasting time on useless things, I have to stop it.
+
 
 //2.Now what is my weakness?
 //-->I don't have Job
@@ -10,10 +14,42 @@
 //-->I can work hard
 
 //4.What can I do to improve myself?
-//-->Code daily and Build something in Quant Strategy
-//-->Apply for jobs daily
-//-->Save money(Trading use only a small portion of money)
+//--->Code daily and Build something in Quant Strategy
+//--->Apply for jobs daily
+//--->Save money(Trading use only a small portion of money)
+//--->I have to work atleast 10 hours daily(coding c++/python, apply for jobs, trading in same time range everyday)
+//--->To busy to in your work, so that I can't think about other things
 
+//I have to believe in myself.
+//and start now.
+
+//What is my plan?
+//1.Improve c++ and python skills
+//2.Leetcode for DSA
+//3.Give specific time for Trading 
+
+///Do it every day
+//set your daily targets in (coding and trading)
+
+//I am really demotivated now, but I have to do it.
+//1.Be Quiet and work hard
+//2.Focus on your goals
+//3.Don't waste time on social media
+//4.Improve your skills as slowly as possible
+//5.don't overthink just do it
+//6.Take care of your health
+//7.Believe in yourself
+//8.stay positive
+//9.Happy mind happy life
+
+
+//Time:-(Stochastic)
+//7:00 - 10:00 Leetcode 
+//lunch break
+//10:00 - 2:00 C++ learning
+//2:00  - 5:00 Python(ml/dl, data analysis)
+//5:00  - 7:00 Stocastic, Mathematics, Astrophysics
+//7:00  - 9:00 Trading
 
 #include<iostream>
 
@@ -21,3 +57,4 @@ int main() {
     std::cout << "C++ Standard Version: " << __cplusplus << std::endl;
     return 0;
 }
+
