aa04744c60023566924d082930e1e1ebba8fc910
Mritunjay Maddhesiya <mritunjay112002@gmail.com>
2026-01-24 20:33:42 +0530

    working on template design pattern

---
+++ LLD/DesignPattern/10_TemplateDP.cpp
@@ -0,0 +1,19 @@
+//1.Specific design pattern
+//2.Follow the piplines
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+//ModelTrainer(load(path), preprocess(data), train(data), evaluate(), save()), templateMethod{})
+//
+//
+
+
+
+
+
+
+
+
+
